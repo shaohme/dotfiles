38b0ed144cd8abd33f3d3f80803a7bdd8110d242
Martin Kjær Jørgensen <me@lagy.org>
2024-06-12 15:50:40 +0200

    bump

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -308,6 +308,19 @@ With argument, do this that many times."
 (add-to-list 'auto-mode-alist '("Makefile.*" . makefile-mode))
 
 
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (concat "Do you really want to delete file " filename " ?"))
+            (progn
+              (delete-file filename)
+              (message "Deleted file %s." filename)
+              (kill-buffer)))
+      (message "Not a file visiting buffer!"))))
+
+
 ;; --- tramp
 (require 'tramp)
 (require 'tramp-sh)
